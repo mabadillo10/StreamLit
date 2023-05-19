4f24cff2f292af38f53a808e7cf4912fc86fa300
mabadillo10 <133566634+mabadillo10@users.noreply.github.com>
2023-05-19 14:40:59 +0800

    Update forecast_streamlit.py

--- forecast_streamlit.py
+++ forecast_streamlit.py
@@ -47,15 +47,17 @@ def build_model(X, y):
 def forecast_data(model, last_x, scaler):
     future_data = []
     num_days = st.number_input('Enter the number of day/s to forecast:', min_value=0, max_value=31, value=0, step=1) # Number of previous days to use for prediction
-    if num_days > 0:
-            # Wait for user to input forecast lookback
-        while st.button('Forecast') == False:
-             pass
-    for i in range(numdays*24):
-        prediction = model.predict(np.array([last_x]))
-        future_data.append(prediction[0])
-        last_x = np.concatenate((last_x[1:], prediction), axis=0)
-
+    try:
+        if num_days > 0:
+                # Wait for user to input forecast lookback
+            while st.button('Forecast') == False:
+                 pass
+        for i in range(numdays*24):
+            prediction = model.predict(np.array([last_x]))
+            future_data.append(prediction[0])
+            last_x = np.concatenate((last_x[1:], prediction), axis=0)
+    except NameError:
+        print("Please enter number of days")
     future_data = np.array(future_data)
     future_data = scaler.inverse_transform(future_data)
     return future_data
